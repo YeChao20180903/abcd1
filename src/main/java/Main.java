106ac0c9d1b41a207add770d2ecac6cc7deb9ee3
yechao <18143478554@163.com>
2023-01-02 19:58:49 +0800

    fifth

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("嘿，您猜怎么着？");
         System.out.println("路易斯的妹妹她够意思");
-        System.out.println("够地道");
     }
 }
