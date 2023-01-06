589bf8a38665907fdd8e18363ab7a5ab70900757
yechao <18143478554@163.com>
2023-01-06 13:27:58 +0800

    eighth

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("嘿，您猜怎么着？");
-        System.out.println("路易斯的妹妹她够意思");
-        System.out.println("test");
+        System.out.println("胖森邀您共领会");
     }
 }
